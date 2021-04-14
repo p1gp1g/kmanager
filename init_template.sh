c482bc52b8b9e9b242dbb62ed533367e557be054
S1m <git@sgougeon.fr>
2021-04-14 22:27:49 +0200

    firstboot.sh

--- init_template.sh
+++ init_template.sh
@@ -23,6 +23,7 @@ virt-sysprep -d template-$OS_VARIANT \
   --append-line "/etc/rc.d/rc.local:[ -x /rw/rc.local ] && /rw/rc.local" \
   --append-line "/etc/rc.d/rc.local:/usr/bin/systemctl enable --now sshd #1del" \
   --append-line "/etc/rc.d/rc.local:/usr/sbin/restorecon /home/user/.ssh/authorized_keys #1del" \
+  --append-line "/etc/rc.d/rc.local:[ -f /rw/firstboot.sh ] && /bin/bash /rw/firstboot.sh && /usr/bin/rm /rw/firstboot.sh #1del" \
   --append-line "/etc/rc.d/rc.local:sed -i '"'/#1del$/d'"' /etc/rc.d/rc.local #1del" \
 
 pstep "Undefining template-$OS_VARIANT"
@@ -30,16 +31,21 @@ pstep "Undefining template-$OS_VARIANT"
 
 pstep "Adding ssh authorized keys"
 mkdir -p ./mnt_rw ./mnt_sys
-guestmount -a "$TEMPLATE_DIR/$RW_TEMPLATE" -m /dev/sda1:/:subvol=home -o uid=$(id -u) -o gid=$(id -g) ./mnt_rw
+guestmount -a "$TEMPLATE_DIR/$RW_TEMPLATE" -m /dev/sda1 -o uid=$(id -u) -o gid=$(id -g) ./mnt_rw
 guestmount -a "$TEMPLATE_DIR/$SYS_TEMPLATE" -m /dev/sda2:/:subvol=home ./mnt_sys
-cp -r ./mnt_sys/user ./mnt_rw/user
-chown -R 1000:1000 ./mnt_rw/user
-mkdir -p ./mnt_rw/user/.ssh
-cp "$HOME/.ssh/$SSH_KEY.pub" ./mnt_rw/user/.ssh/authorized_keys
-chown -R 1000:1000 ./mnt_rw/user/.ssh
-chmod -R 500 ./mnt_rw/user/.ssh
+cp -r ./mnt_sys/user ./mnt_rw/home/user
+chown -R 1000:1000 ./mnt_rw/home/user
+mkdir -p ./mnt_rw/home/user/.ssh
+cp "$HOME/.ssh/$SSH_KEY.pub" ./mnt_rw/home/user/.ssh/authorized_keys
+chown -R 1000:1000 ./mnt_rw/home/user/.ssh
+chmod -R 500 ./mnt_rw/home/user/.ssh
 
-guestunmount ./mnt_rw
+if [ -f "./firstboot.sh" ]; then
+pstep "Uploading firstboot.sh"
+cp ./firstboot.sh ./mnt_rw/rw/
+fi
+
 guestunmount ./mnt_sys
+guestunmount ./mnt_rw
 
 pstep "Done!"
